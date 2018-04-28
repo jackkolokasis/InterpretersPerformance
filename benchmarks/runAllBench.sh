2f6e69fa0595bc8ccbc596a01cffd381d34707eb
Manos <manospavl@ics.forth.gr>
2018-04-28 15:47:52 +0300

    midified to get num of iteration

--- benchmarks/runAllBench.sh
+++ benchmarks/runAllBench.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-
-#directory=$1
-#cmd=$2
-#ext=$3
 architecture=$1
-
-dacapoArgs=("avrora" "batik" "h2" "sunflow" "tomcat" "tradebeans" \
-"tradesoap" "eclipse" "fop" "jython" "luindex" "lusearch" "pmd" \
-"sunflow" "tomcat" "tradebeans" "tradesoap" "xalan")
+iteration=$2
 
 if [ $# -eq 0 ]
   then
       echo "./runAllBench <architecture>"
       echo "Add architecture (intel/amd)."
+      echo "Add add iteration id (0..30)."
       exit
 fi
-
+ 
 for f in python/performance/benchmarks/*.py
 do
     echo ${f}
     filename="${f##*/}"
     echo ${filename}
-    { time ./executeOProf.sh python ${f} ${architecture} > out_${filename}.txt ; } 2>> out_${filename}.txt 
+    { time ./executeOProf.sh python ${f} ${architecture} > \
+	    out_${filename}_${iteration}.txt ; } 2>> out_${filename}_${iteration}.txt 
+		exit
 done
 cd javascript/octane/     
 for f in run_*.js
@@ -30,13 +26,19 @@ do
     filename="${f##*/}"
     echo ${filename}
     { time ../../executeOProf.sh rhino ${f} ${architecture} > \
-        ../../out_${filename}.txt ; } 2>> ../../out_${filename}.txt 
+        ../../out_${filename}_${iteration}.txt ; } 2>> \
+   		 ../../out_${filename}_${iteration}.txt 
 done
 cd -
+
+dacapoArgs=("avrora" "batik" "h2" "sunflow" "tomcat" "tradebeans" \
+"tradesoap" "eclipse" "fop" "jython" "luindex" "lusearch" "pmd" \
+"sunflow" "tomcat" "tradebeans" "tradesoap" "xalan")
+
 
 for i in "${dacapoArgs[@]}"
 do
     echo ${i}
-    { time ./executeOProf.sh java ${i} ${architecture} > out_${i}.txt ; } 2>> out_${i}.txt 
-        exit
+    { time ./executeOProf.sh java ${i} ${architecture} > \
+	    out_${i}_${iteration}.txt ; } 2>> out_${i}_${iteration}.txt 
 done
