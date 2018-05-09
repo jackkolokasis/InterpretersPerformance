cd3c7d87fa8d6d65a60d2fdd069f2314104f0e5e
Manos <manospavl@ics.forth.gr>
2018-05-09 20:29:09 +0300

    renamed

--- benchmarks/cleanAll.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm help.out
-rm results.log
-rm javascript/octane/help.out
-rm -rf python/performance/benchmarks/__pycache__/bm_regex_effbot.cpython-36.pyc
-rm -rf python/performance/benchmarks/__pycache__/bm_regex_v8.cpython-36.pyc
