aa6de5ba0e0b386390e53db678031c5dfa12adec
jackkolokasis <jackkolokasis19@gmail.com>
2018-05-12 14:47:29 +0300

    Parse and merge results

--- benchmarks/2.parseResutls.sh
+++ benchmarks/2.parseResutls.sh
@@ -13,53 +13,30 @@
 # @brief  This script parse all the outputs metrics from the
 # benchmarks
 # Usage 
-#      ./parseResults.sh <iteration num> <architecture>
+#      ./parseResults.sh
 # 
 ###################################################
 
 
-architecture=$1     # Architecture version
-iterationNum=$2     # Number of iterations
+iterationNum=10     # Number of iterations
 
-# Check input arguments
-usage ()
-{
-        echo "./parseResults <architecture> <iteration_number>"
-        echo "Add architecture (core2/haswell/ivy_bridge/nehalem/amd)."
-        echo "Add number of iterations."
-        exit
-}
+declare -a archs=("amd" "core2" "haswell" "ivy_bridge" "nehalem") 
 
 # Parse all the results to csv
 parser () {
-
+    
     # Directory containg benchmarks metrics for python
-    RESULTDIR=updateRes/${architecture}/$1 
+    RESULTDIR="updateRes/$3/$1"
 
     # Create csv output file
-    OUTPUTFILE=updateRes/${architecture}/$2
+    OUTPUTFILE="updateRes/$3/$2"
     echo -n "" > ${OUTPUTFILE}
 
     # Print Header row for csv files. 
     # Different header row for the state of AMD
     echo -ne "Benchmark;" >> ${OUTPUTFILE}
 
-    if [ ${architecture} = "amd" ]
-    then
-        for ((i=0; i<${iterationNum}; i++))
-        do
-            echo -ne "TOTAL_INSTR;TOTAL_BR_RETIRED;" >> ${OUTPUTFILE}
-            echo -ne "TOTAL_BR_MISS_RETIRED;TOTLA_MISS_IND_BR;" >> ${OUTPUTFILE}
-        done
-    else
-        for ((i=0; i<${iterationNum}; i++))
-        do
-            echo -ne "TOTAL_INSTR;TOTAL_BR_RETIRED;" >> ${OUTPUTFILE}
-            echo -ne "TOTAL_BR_MISS_RETIRED;TOTAL_BR;" >> ${OUTPUTFILE}
-            echo -ne "TOTAL_MISS_PRED;TOTAL_IND_BR;TOTAL_MISS_IND_BR;" >> ${OUTPUTFILE}
-            echo -ne "TOTAL_COND_BR;TOTAL_MISS_COND_BR;" >> ${OUTPUTFILE}
-        done
-    fi
+    echo -ne "MPKI;MPKB;" >> ${OUTPUTFILE}
 
     # Change line
     echo " " >> ${OUTPUTFILE}
@@ -75,6 +52,8 @@ parser () {
         benchName=`echo ${fnameNoIter} | awk -F 'out_' '{print ($2)}'`
 
         # Iterate all benchmarks and get brach predictions metrics
+        total_MPKI=0
+        total_MPKB=0
         for ((i=0; i<${iterationNum}; i++))
         do
             if [ ${i} -eq 0 ]; then
@@ -86,64 +65,84 @@ parser () {
             # the 2nd column containing the metrics.  Assign metrics to
             # variables as the same order in the file
             #
-            # Grep output examples: --------------------- Count 297,815,131,747
+            # Grep output examples: --------------------- 
+            # Count 297,815,131,747
             # 883,770,778 59,123,344,075 349,815,341 7,495,202,690
             # 38,440,604,214 371,647,733 43,580,826,438 302,303,658
             # ---------------------
             #
 
-            if [ ${architecture} != "amd" ]
+            if [ $3 != "amd" ]
             then
                 grepLines=$(cat ${RESULTDIR}/${fnameNoIter}\:${i}.txt \
                     | grep -A 9 "% time counted" \
                     | awk '{print$2}')
 
-                total_instr=`echo ${grepLines} | cut -d ' ' -f2`
-                miss_pred=`echo ${grepLines} | cut -d ' ' -f3`
-                total_branches=`echo ${grepLines} | cut -d ' ' -f4`
-                miss_indirect_br=`echo ${grepLines} | cut -d ' ' -f5`
-                indirect_br=`echo ${grepLines} | cut -d ' ' -f6`
-                conditional_br=`echo ${grepLines} | cut -d ' ' -f7`
-                miss_conditional_br=`echo ${grepLines} | cut -d ' ' -f8`
-                total_branches_retired=`echo ${grepLines} | cut -d ' ' -f9`
-                total_branches_miss_retired=`echo ${grepLines} | cut -d ' ' -f10`
+                total_instr=`echo ${grepLines} | cut -d ' ' -f2 | sed -e 's,\,,,g'`
+                miss_pred=`echo ${grepLines} | cut -d ' ' -f3 | sed -e 's,\,,,g'`
+                total_branches=`echo ${grepLines} | cut -d ' ' -f4 | sed -e 's,\,,,g'`
+                miss_indirect_br=`echo ${grepLines} | cut -d ' ' -f5 | sed -e 's,\,,,g'`
+                indirect_br=`echo ${grepLines} | cut -d ' ' -f6 | sed -e 's,\,,,g'`
+                conditional_br=`echo ${grepLines} | cut -d ' ' -f7 | sed -e 's,\,,,g'`
+                miss_conditional_br=`echo ${grepLines} | cut -d ' ' -f8 | sed -e 's,\,,,g'`
+                total_branches_retired=`echo ${grepLines} | cut -d ' ' -f9 | sed -e 's,\,,,g'`
+                total_branches_miss_retired=`echo ${grepLines} | cut -d ' ' -f10 | sed -e 's,\,,,g'`
 
-                echo -ne "${total_instr};${total_branches_retired};" >> ${OUTPUTFILE}
-                echo -ne "${total_branches_miss_retired};" >> ${OUTPUTFILE}
-                echo -ne "${total_branches};${miss_pred};" >> ${OUTPUTFILE}
-                echo -ne "${indirect_br};${miss_indirect_br};" >> ${OUTPUTFILE}
-                echo -ne "${conditional_br};${miss_conditional_br};" >> ${OUTPUTFILE}
+                # Calculation of MPKI. 
+                thousand_Instr=$(echo "scale=4; ${total_instr} / 1000" | bc)
+                mpki=$(echo "scale=4; ${miss_indirect_br} / ${thousand_Instr}" | bc)
+                total_MPKI=$(echo "scale=4;  ${total_MPKI} + ${mpki}" | bc)
+                
+                # Calculation of MPKB. 
+                thousand_Br=$(echo "scale=4; ${total_branches} / 1000" | bc)
+                mpkb=$(echo "scale=4; ${miss_indirect_br} / ${thousand_Br}" | bc)
+                total_MPKB=$(echo "scale=4;  ${total_MPKB} + ${mpkb}" | bc)
+
             else
                 grepLines=$(cat ${RESULTDIR}/${fnameNoIter}\:${i}.txt \
                     | grep -A 4 "% time counted" \
                     | awk '{print$2}')
 
-                total_instr=`echo ${grepLines} | cut -d ' ' -f2`
-                miss_indirect_br=`echo ${grepLines} | cut -d ' ' -f3`
-                total_branches_retired=`echo ${grepLines} | cut -d ' ' -f4`
-                total_branches_miss_retired=`echo ${grepLines} | cut -d ' ' -f5`
+                total_instr=`echo ${grepLines} | cut -d ' ' -f2 | sed -e 's,\,,,g'`
+                miss_indirect_br=`echo ${grepLines} | cut -d ' ' -f3 | sed -e 's,\,,,g'`
+                total_branches_retired=`echo ${grepLines} | cut -d ' ' -f4 | sed -e 's,\,,,g'`
+                total_branches_miss_retired=`echo ${grepLines} | cut -d ' ' -f5 | sed -e 's,\,,,g'`
 
-                echo -ne "${total_instr};${total_branches_retired};" >> ${OUTPUTFILE}
-                echo -ne "${total_branches_miss_retired};" >> ${OUTPUTFILE}
-                echo -ne "${miss_indirect_br};" >> ${OUTPUTFILE}
+                # Calculation of MPKI. 
+                thousand_Instr=$(echo "scale=4; ${total_instr} / 1000" | bc)
+                mpki=$(echo "scale=4; ${miss_indirect_br} / ${thousand_Instr}" | bc)
+                total_MPKI=$(echo "scale=4;  ${total_MPKI} + ${mpki}" | bc)
+                
+                # Calculation of MPKB. 
+                thousand_Br=$(echo "scale=4; ${total_branches_retired} / 1000" | bc)
+                mpkb=$(echo "scale=4; ${miss_indirect_br} / ${thousand_Br}" | bc)
+                total_MPKB=$(echo "scale=4;  ${total_MPKB} + ${mpkb}" | bc)
+
             fi
         done
+
+        # Calculation of the average
+        avg_MPKI=$(echo "scale=4;  ${total_MPKI} / 10" | bc)
+        avg_MPKB=$(echo "scale=4;  ${total_MPKB} / 10" | bc)
+        
+        # Print the average 
+        echo -ne "${avg_MPKI};${avg_MPKB}" >> ${OUTPUTFILE}
         echo " " >> ${OUTPUTFILE}
     done
 }
 
-# Check input arguments
-if [ $# -lt 2 ]
-then
-    usage
-fi
+for ar in "${archs[@]}"
+do
+    # Python Results
+    parser pythonRes merge_pythonRes.csv ${ar}
 
-# Python Results
-parser pythonRes merge_pythonRes.csv
+    # Javascript Results
+    parser javascriptRes merge_javascriptRes.csv ${ar}
 
-# Javascript Results
-parser javascriptRes merge_javascriptRes.csv
+    # Java Results
+    parser javaRes merge_javaRes.csv ${ar}
+done
 
-# Java Results
-parser javaRes merge_javaRes.csv
-
+# Merge results
+./merge_csv.sh
+exit
---
+++ benchmarks/merge_csv.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+###################################################
+#
+# file: mege_csv.sh
+#
+# @Author   Iacovos G. Kolokasis
+#           Manos Pavlidakis
+# @Version  12-05-2018
+# @email    kolokasis@ics.forth.gr
+#           manospavl@ics.forth.gr
+#
+# @brief  This script merges all interpeters outputs results
+# Usage 
+#      ./merge_csv.sh 
+# 
+###################################################
+
+echo -n "" >.tmp.csv
+declare -a archs=("amd" "core2" "haswell" "ivy_bridge" "nehalem") 
+declare -a inter=("python" "java" "javascript")
+
+# Iterate all interpreters and merge all results to one
+for j in "${inter[@]}"
+do
+    paste -d ";" updateRes/${archs[0]}/merge_${j}Res.csv updateRes/${archs[1]}/merge_${j}Res.csv \
+        updateRes/${archs[2]}/merge_${j}Res.csv updateRes/${archs[3]}/merge_${j}Res.csv \
+        updateRes/${archs[4]}/merge_${j}Res.csv > .tmp2.csv
+    
+    tail -n +2 .tmp2.csv > .tmp.csv
+
+    echo -ne "Bench " > compareResults/inputs/${j}_allArchs_tmp.csv
+
+    for i in "${archs[@]}"
+    do
+        echo -ne "MPKI_${i} MPKB_${i} " >> compareResults/inputs/${j}_allArchs_tmp.csv
+    done
+
+    echo "" >> compareResults/inputs/${j}_allArchs_tmp.csv
+
+    cat .tmp.csv | sed "s,\ ,,g" |awk -F';' '{print $1 " " $2 " " $3 " " $5 " " $6 " " $8 " " $9 " " $11 " " $12 " " $14 " " $15}' \
+        >> compareResults/inputs/${j}_allArchs_tmp.csv
+
+    head -n -1 compareResults/inputs/${j}_allArchs_tmp.csv > compareResults/inputs/${j}_allArchs.csv
+
+    rm -rf compareResults/inputs/${j}_allArchs_tmp.csv .tmp2.csv .tmp.csv
+done
